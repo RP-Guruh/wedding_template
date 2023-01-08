96478c1da7b9425a2c459c5eeaab99bb764764cb
rp-guruh <github.gurh@gmail.com>
2023-01-08 21:26:28 +0700

    fix

--- js/effect.js
+++ js/effect.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
     $("#flipGift").click(function(){
-      $(".panelGift1").slideDown("slow");
+      $(".panelGift1").slideDown("1000");
+      $(".panelGift2").slideDown("1000");
     });
-  });
+
+    $("#paste1").click(function() {
+      var temp = $("<input>");
+      $("body").append(temp);
+      temp.val($('.norek1').text()).select();
+      document.execCommand("copy");
+      temp.remove();
+      $("#paste1").text("Tersalin").css("color", "white");
+    });
+
+    $("#paste2").click(function() {
+      var temp = $("<input>");
+      $("body").append(temp);
+      temp.val($('.norek2').text()).select();
+      document.execCommand("copy");
+      temp.remove();
+      $("#paste2").text("Tersalin").css("color", "white");
+    })
+});
