ac06e87c8239801f0fb192b1c701968d7133d1b2
RP-Guruh <github.guruh@gmail.com>
2023-01-10 08:33:14 +0700

    bikin form kehadiran

--- js/effect.js
+++ js/effect.js
@@ -1,32 +1,41 @@
 $(document).ready(function(){
-    $("#flipGift").click(function(){
-      $(".panelGift1").slideToggle("1000");
-      $(".panelGift2").slideToggle("1000");
-    });
+  $("#flipGift").click(function(){
+    $(".panelGift1").slideToggle("1000");
+    $(".panelGift2").slideToggle("1000");
+  });
 
-    $("#bukaUndangan").click(function(){
-      $(".section-landing").slideDown("2000");
-      $(".bg_list_pengantin").slideDown("2000");
-      $(".bg-card").slideDown("2000");
-      $("#bukaUndangan").css("color", "white");
-    });
+  $("#bukaUndangan").click(function(){
+    $(".section-landing").slideDown("2000");
+    $(".bg_list_pengantin").slideDown("2000");
+    $(".bg-card").slideDown("2000");
+    $("#bukaUndangan").css("color", "white");
+  });
 
 
-    $("#paste1").click(function() {
-      var temp = $("<input>");
-      $("body").append(temp);
-      temp.val($('.norek1').text()).select();
-      document.execCommand("copy");
-      temp.remove();
-      $("#paste1").text("Tersalin").css("color", "white");
-    });
+  $("#paste1").click(function() {
+    var temp = $("<input>");
+    $("body").append(temp);
+    temp.val($('.norek1').text()).select();
+    document.execCommand("copy");
+    temp.remove();
+    $("#paste1").text("Tersalin").css("color", "white");
+  });
 
-    $("#paste2").click(function() {
-      var temp = $("<input>");
-      $("body").append(temp);
-      temp.val($('.norek2').text()).select();
-      document.execCommand("copy");
-      temp.remove();
-      $("#paste2").text("Tersalin").css("color", "white");
-    })
+  $("#paste2").click(function() {
+    var temp = $("<input>");
+    $("body").append(temp);
+    temp.val($('.norek2').text()).select();
+    document.execCommand("copy");
+    temp.remove();
+    $("#paste2").text("Tersalin").css("color", "white");
+  });
+
+  $('.pilihKehadiran').change(function() {
+    if ($(this).val() == 'Hadir') {
+      $(".jumlahTamu").slideDown("2000");
+    }    
+    else {
+      $(".jumlahTamu").slideUp("2000");
+    }     
+});
 });
